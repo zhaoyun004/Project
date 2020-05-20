4b4b0a86bcaa6fbd04417b581605ad8bfe9492bf
zhangyun007 <zhaoyun004@qq.com>
2020-05-20 10:15:42 +0800

    mod

--- scheme.s
+++ scheme.s
@@ -4,15 +4,20 @@
 
 (if (< i 19) (print (+ i 1)))
 
-(while (< i 23) (begin (print i) (set i (+ i 1))(if (eq? i 20) break)))
+(while (< i 23) (begin 
+    (print i) 
+    (set i (+ i 1))
+    (if (eq? i 20) break))
+ )
 
-(for (set i 23) (< i 45) (set i (+ i 2)) (begin (print i) (if (eq? i 43) break)))
-
+(for (set i 23) (< i 45) (set i (+ i 2)) (begin 
+    (print i) 
+    (if (eq? i 43) break))
+)
 
 (set sum 0)
 (for-each (lambda (x) (set sum (+ sum x))) (list 3 4 5 6 7 22))
 (print sum)
-
 
 (set abc 123)
 
--- test.s
+++ test.s
@@ -1,12 +1,19 @@
 (set i 12)
 
-(if (< i 19) (print (+ i 1))
-    )
+(if (< i 19) (print (+ i 1)))
     
-(while (< i 23) (begin (print i) (set i (+ i 1))(if (eq? i 19) break)))
+(while (< i 23) (begin 
+    (print i) 
+    (set i (+ i 1))
+    (if (eq? i 19) 
+        break)
+    )
+)
 
 (set s 0)
-(for (set i 0) (< i 101) (set i (+ i 1)) (set s (+ s i)))
+(for (set i 0) (< i 101) (set i (+ i 1)) 
+    (set s (+ s i))
+)
 (print s)
    
 (define (sum x r)
